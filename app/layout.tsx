57c14abc86adf2e6740f2e803c0b3790a3525fa7
avee <aveechakra@gmail.com>
2025-05-07 01:38:31 +0600

    layout name changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Barnick Invoice Application',
+  title: 'Barnick Invoice',
   description: 'Created by Barnick Pracharani',
   generator: 'v0.dev',
 }
